ccd62c3d9eeddcfcb453eb24a4838343ddfcf227
jungeun-p <mrk7753@naver.com>
2023-02-01 15:48:04 +0900

    [ modify ] Apply application to react-router-dom
    - Create Layout Component
    - Create Page Folder and Main, Detail, Search Component
    - Apply App Component to BrowserRouter Component

--- api-service-app/src/App.js
+++ api-service-app/src/App.js
@@ -1,42 +1,32 @@
-import requests from "./api/requests";
-import Banner from "./components/Banner";
+
 import Nav from "./components/Nav";
-import Row from "./components/Row";
 import "./App.css";
 import Footer from "./components/Footer";
+import { Outlet, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
+
+  const Layout = () => {
+    return(
+      <div>
+        <Nav />
+        <Outlet />
+        <Footer />
+      </div>
+    )
+  }
   return (
     <div className="app">
-      <Nav />
-      <Banner />
-      <Row 
-        title="NETFLIX ORIGINALS" 
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row 
-        title="Trending Now"
-        id="TN"
-        fetchUrl={requests.fetchTrending}
-      />
-      <Row 
-        title="Top Rated"
-        id="TR"
-        fetchUrl={requests.fetchTopRated}
-      />
-      <Row 
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row 
-        title="Comedy Movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
