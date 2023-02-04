f4937f7b8b9ae7cab9534086e40e2c09a5f9cc2e
jungeun-p <mrk7753@naver.com>
2023-02-04 17:41:29 +0900

    [ modify ] Create function return search UI component
    - renderSearchResults from SearchPage
    - apply SearchPage to styling with css file

--- api-service-app/src/pages/SearchPage/index.js
+++ api-service-app/src/pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import axios from '../../api/axios';
+import "./SearchPage.css";
 
 export default function SearchPage() {
     const [searchResults, setSearchResults] = useState([]);
@@ -23,10 +24,36 @@ export default function SearchPage() {
             console.log(`error, ${error}`);
         }
     }
+    
+    // rendering function
+    const renderSearchResults = () => {
+        return searchResults.length > 0 ? (
+            <section className="search-container">
+                {searchResults.map((movie) => {
+                    if(movie.backdrop_path !== null && movie.media_type !== "person") {
+                        const movieImageUrl = 
+                        "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+                        return (
+                            <div className="movie" key={movie.id}>
+                                <div className="movie__column-poster">
+                                    <img 
+                                    src={movieImageUrl} 
+                                    alt="movie" 
+                                    className="movie__poster" />
+                                </div>
+                            </div>
+                        )
+                    }
+                })}
+            </section>
+        ):(<section className="no-results">
+            <div className="no-results__text">
+                <p>
+                    No movie match {searchTerm} you want search.
+                </p>
+            </div>
+        </section>)
+    }
 
-    return (
-        <div>
-            Search
-        </div>
-    )
+    return renderSearchResults();
 }
