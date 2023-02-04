6eba4149a19eed2c485add762ddaa385fe0c27d1
jungeun-p <mrk7753@naver.com>
2023-02-04 17:16:00 +0900

    [ modify ] Create fetchSearchMovie api function with useEffect

--- api-service-app/src/pages/SearchPage/index.js
+++ api-service-app/src/pages/SearchPage/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
+import axios from '../../api/axios';
 
 export default function SearchPage() {
-    
+    const [searchResults, setSearchResults] = useState([]);
+
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
     }
     let query = useQuery();
     const searchTerm = query.get("q"); // query q의 값
+
+    useEffect(()=>{
+        if(searchTerm) fetchSearchMovie(searchTerm);
+    }, [searchTerm])
+    
+    const fetchSearchMovie = async(searchTerm) => {
+        try {
+            const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+            setSearchResults(request.data.results);
+        } catch (error) {
+            console.log(`error, ${error}`);
+        }
+    }
 
     return (
         <div>
