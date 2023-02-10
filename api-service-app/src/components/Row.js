97df71b5bafb2102a4a5aa45f3fe5417488e6b54
jungeun-p <mrk7753@naver.com>
2023-02-10 17:17:03 +0900

    [ modify ] Update Swiper props and Styling with CSS

--- api-service-app/src/components/Row.js
+++ api-service-app/src/components/Row.js
@@ -3,6 +3,17 @@ import React, { useEffect, useState } from 'react'
 import './Row.css';
 import MovieModal from './MovieModal';
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 export default function Row({ title, fetchUrl, id, isLargeRow }) {
     const [movies, setmovies] = useState([]);
     const [modalOpen, setModalOpen] = useState(false);
@@ -24,38 +35,45 @@ export default function Row({ title, fetchUrl, id, isLargeRow }) {
 
     return <section className="row">
         <h2>{title}</h2>
-        {/* <div className="slider">
-            <div 
-                className="slider__arrow-left" 
-                onClick={() => {
-                    document.getElementById(id).scrollLeft -= window.innerWidth - 80; // 80은 Row section margin(40), row__posters div padding(40)을 제외하는 값
-                }}>
-                <span className="arrow">
-                    {"<"}
-                </span>
-            </div> */}
+        <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={10}
+            loop={true} // loop 기능
+            breakpoints={{ // view point에 따라 보이는 슬라이드와, 넘어가는 슬라이드 개수
+                1378: {
+                    slidesPerView: 6,
+                    slidesPerGroup: 6,
+                },
+                998: {
+                    slidesPerView: 5,
+                    slidesPerGroup: 5,
+                },
+                625: {
+                    slidesPerView: 4,
+                    slidesPerGroup: 4,
+                },
+                0: {
+                    slidesPerView: 0,
+                    slidesPerGroup: 0,
+                },
+            }}
+            navigation // arrow button 사용
+            pagination={{ clickable: true }} // 페이지 버튼 유무
+        >
             <div id={id} className="row__posters">
                 {movies.map((movie) => (
-                    <img 
-                        key={movie.id}
-                        className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                        src={`https://image.tmdb.org/t/p/original${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-                        alt={movie.name}
-                        onClick={() => handleClick(movie)}
-                    />
+                    <SwiperSlide>
+                        <img 
+                            key={movie.id}
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            src={`https://image.tmdb.org/t/p/original${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                            alt={movie.name} 
+                            onClick={() => handleClick(movie)}
+                        />
+                    </SwiperSlide>
                 ))}
             </div>
-            {/* <div 
-                className="slider__arrow-right"
-                onClick={() => {
-                    document.getElementById(id).scrollLeft += window.innerWidth + 80;
-                }}
-            >
-                <span className="arrow">
-                    {">"}
-                </span>
-            </div>
-        </div> */}
+        </Swiper>
         {modalOpen && 
             <MovieModal 
                 {...movieSelected} // movieSelected={movieSelected}
