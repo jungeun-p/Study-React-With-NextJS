59a8249c1cfd01b843870547aba322bf8cb7d792
jungeun-p <mrk7753@naver.com>
2023-02-14 14:47:25 +0900

    [ create ] create button-app for TDD

--- react-button-app/src/App.test.js
+++ react-button-app/src/App.test.js
@@ -5,9 +5,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-  
-  const lintTest = screen.getByRole('button', {
-    name: 'lintTest'
-  })
-  expect(lintTest.textContent).toBe("lintTest");
 });
