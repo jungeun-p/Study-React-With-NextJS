01e4b6d32c54be71a1ac9f022a7ec6c9317f2b03
jungeun-p <mrk7753@naver.com>
2023-02-14 15:25:03 +0900

    [ modify ] install user-event and TDD for button function
    - testing-library/user-event

--- react-button-app/src/App.test.js
+++ react-button-app/src/App.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 test("the counter starts at 0", () => {
   render(<App />);
@@ -17,4 +18,17 @@ test("plus button has correct text", () => {
   render(<App />);
   const buttonElement = screen.getByTestId("plus-button");
   expect(buttonElement).toHaveTextContent("+");
+});
+
+test("when push + button, the counter changes to 1", () => {
+  render(<App />);
+  // + button
+  const buttonElement = screen.getByTestId("minus-button");
+  // push + button
+  // fireEvent.click(buttonElement);
+  userEvent.click(buttonElement);
+  // counter
+  const counterElement = screen.getByTestId("counter");
+  // counter changes to "1";
+  expect(counterElement).toHaveTextContent(1);
 });
