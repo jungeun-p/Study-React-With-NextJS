dacd4c385a0700b508aedfd5ddc2d4261c0003ac
jungeun-p <mrk7753@naver.com>
2023-02-17 23:49:58 +0900

    [ modify ] create redux and store
    - src/reducers
    - createStore and apply reducers from App.tsx

--- counter-app/src/index.tsx
+++ counter-app/src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import counter from './reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+); 
+
+const store = createStore(counter);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App 
+      value={store.getState()}
+     />
   </React.StrictMode>
 );
 
