8fcaa48ca3432b25e33d2f838adf8c5af9ed18dd
jungeun-p <mrk7753@naver.com>
2023-02-24 15:56:05 +0900

    [ modify ] Add Delete Post function
    - Create Delete input checkbox when check the Post
    - Create deletePost function
    - using 'use client'

--- nextjs13-app/app/posts/PostItem.tsx
+++ nextjs13-app/app/posts/PostItem.tsx
@@ -1,13 +1,34 @@
+'use client';
+
 import Link from "next/link";
 import React from "react";
+import postStyles from "@/styles/Home.module.css";
+import { useRouter } from "next/navigation";
 
 const PostItem = ({ post }: any) => {
   const { id, title, created } = post || {};
+  
+  const router = useRouter();
+  const deletePost = async (id: string) => {
+    await fetch(`http://127.0.0.1:8090/api/collections/posts/records/${id}`,
+    {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      cache: "no-store",
+    });
+    router.refresh();
+  }
   return (
     <div>
-      <Link href={`/posts/${id}`}>
-        <h3>{title}</h3>
-      </Link>
+      <div className={postStyles.content}>
+        <input 
+          type="checkbox"
+          onChange={() => deletePost(id)}
+        />
+        <Link href={`/posts/${id}`}>
+          <h3>{title}</h3>
+        </Link>
+      </div>
     </div>
   );
 };
