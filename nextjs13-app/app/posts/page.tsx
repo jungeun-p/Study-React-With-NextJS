701c4d3d3b36e5d6381aa7822882a36f6dee1e47
jungeun-p <mrk7753@naver.com>
2023-02-24 15:54:54 +0900

    [ modify ] Add Create Post Function
    - Add CreatePost component
    - Add HandleSubmit function for create post and return form tag

--- nextjs13-app/app/posts/page.tsx
+++ nextjs13-app/app/posts/page.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import PostItem from './PostItem';
+import React from "react";
+import { CreatePost } from "./CreatePost";
+import PostItem from "./PostItem";
 
-const getPosts = async() => {
-    const res = await fetch('http://127.0.0.1:8090/api/collections/posts/records', { 
-        cache: 'no-store' 
-    });
-    const data = await res.json();
-    return data?.items as any[];
-}
+const getPosts = async () => {
+  const res = await fetch(
+    "http://127.0.0.1:8090/api/collections/posts/records",
+    {
+      cache: "no-store",
+    }
+  );
+  const data = await res.json();
+  return data?.items as any[];
+};
 
 const PostPage = async () => {
-    const posts = await getPosts();
-    return (
-        <div>
-            <h1>Posts</h1>
-            {posts?.map((post) => {
-                return <PostItem key={post.id} post={post} />
-            })}
-        </div>
-    )
-}
+  const posts = await getPosts();
+  return (
+    <div>
+      <h1>Posts</h1>
+      {posts?.map((post) => {
+        return <PostItem key={post.id} post={post} />;
+      })}
+      <CreatePost />
+    </div>
+  );
+};
 
 export default PostPage;
