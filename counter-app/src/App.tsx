f2728e81c62ecc762acca384bbecaa4dc13424e9
jungeun-p <mrk7753@naver.com>
2023-02-28 13:29:05 +0900

    [ modify ] Update createStore parameter (counter to rootReducer)
    - Test Dispatch Action type TODO_ADD

--- counter-app/src/App.tsx
+++ counter-app/src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 type Props = {
-  value: number;
+  value: any;
   onIncrement: () => void;
   onDecrement: () => void;
 };
@@ -11,7 +11,7 @@ type Props = {
 function App({ value, onIncrement, onDecrement }: Props) {
   return (
     <div className="App">
-      Clicked: {value}
+      {/* Clicked: {value} */}
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
     </div>
