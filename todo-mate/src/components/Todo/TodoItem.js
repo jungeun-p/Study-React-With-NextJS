f19e663f2167e22a28e10647bfe134f19c5e0381
jungeun-p <mrk7753@naver.com>
2023-03-17 17:46:12 +0900

    [ modify ] Update TodoItem Component
    - update jsx component
    - apply TodoItem to styling from TodoItem.css file
    - background color update

--- todo-mate/src/components/Todo/TodoItem.js
+++ todo-mate/src/components/Todo/TodoItem.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./TodoItem.css";
 
-const TodoItem = () => {
-  return <div>TodoItem</div>;
+const TodoItem = ({ id, date, title, contents, completed }) => {
+  return (
+    <div className={`todoItemContainer ${id % 2 === 1 ? "black" : "white"}`}>
+      <div className="topWrapper">
+        <div className="titleWrapper">
+          <input
+            className="todoCompleted"
+            type="checkbox"
+            name="completed"
+            checked={completed}
+            onChange={() => {}}
+          />
+          <div className="todoTitle">{title}</div>
+        </div>
+        <div className="todoContents">{contents}</div>
+      </div>
+      <div className="bottomWrapper">
+        <div className="todoDate">{date}</div>
+      </div>
+    </div>
+  );
 };
 
 export default TodoItem;
