706cc4dd8ada82f777f1c4c9f4b85dd3ec5c277a
jungeun-p <mrk7753@naver.com>
2023-03-17 17:44:58 +0900

    [ modify ] Update TodoList Component
    - Import TodoItem Component
    - Apply TodoList to styling TodoList.css

--- todo-mate/src/components/Todo/TodoList.js
+++ todo-mate/src/components/Todo/TodoList.js
@@ -1,7 +1,22 @@
 import React from "react";
+import TodoItem from "./TodoItem";
+import './TodoList.css';
 
-const TodoList = () => {
-  return <div>TodoList</div>;
+const TodoList = ({ todoData }) => {
+  return (
+    <div className="todoListContainer">
+      {todoData.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          date={todo.date}
+          id={todo.id}
+          title={todo.title}
+          contents={todo.contents}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
