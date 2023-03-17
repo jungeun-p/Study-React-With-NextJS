929e80a17a479ae22a5d58e28580a1772fb2e4ff
jungeun-p <mrk7753@naver.com>
2023-03-17 16:47:14 +0900

    [ modify ] Add Outlet, Routes, Route Component from 'react-router-dom'
    - create Layout function for Outlet
    - import component from nav, todo, mypage folder

--- todo-mate/src/App.js
+++ todo-mate/src/App.js
@@ -1,7 +1,27 @@
+import Nav from "./components/Navigation/Nav";
+import { Outlet, Route, Routes } from "react-router-dom";
+import MainPage from "./MainPage";
+import Mypage from "./components/Mypage";
+import Header from "./components/Navigation/Header";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Header />
+        <Outlet />
+        <Nav />
+      </div>
+    );
+  };
+  
   return (
-    <>todo-mate</>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="mypage" element={<Mypage />} />
+      </Route>
+    </Routes>
   );
 }
 
