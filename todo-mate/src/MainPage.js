0bd3380056b2ce0ca9c7ec459093a6b39e12dd85
jungeun-p <mrk7753@naver.com>
2023-03-17 17:43:30 +0900

    [ modify ] Update Mainpage
    - Apply MainPage component to Styling from Mainpage.css
    - Create DummyData
    - Create todoData State

--- todo-mate/src/MainPage.js
+++ todo-mate/src/MainPage.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoForm from "./components/Todo/TodoForm";
 import TodoList from "./components/Todo/TodoList";
+import './MainPage.css';
 
 const MainPage = () => {
+  const initialize = [
+    {
+      id: 1,
+      date: "23-01-11",
+      title: "First",
+      contents: `There's nothing like threatening to go live with a bunch of Bill Murray pictures on a website to 'encourage' your clients to send you their content faster.`,
+      completed: true,
+    },
+    {
+      id: 2,
+      date: "23-01-11",
+      title: "Second",
+      contents: "SecondContents",
+      completed: false,
+    },
+    {
+      id: 3,
+      date: "23-01-11",
+      title: "Third",
+      contents: "ThirdContents",
+      completed: false,
+    },
+    {
+      id: 4,
+      date: "23-01-11",
+      title: "First",
+      contents: "FirstContents",
+      completed: true,
+    },
+    {
+      id: 5,
+      date: "23-01-11",
+      title: "Second",
+      contents: "SecondContents",
+      completed: false,
+    },
+    {
+      id: 6,
+      date: "23-01-11",
+      title: "Third",
+      contents: "ThirdContents",
+      completed: false,
+    },
+  ];
+  const [todoData, setTodoData] = useState(initialize);
   return (
-    <>
+    <div className="mainContainer">
       <TodoForm />
-      <TodoList />
-    </>
+      <TodoList todoData={todoData} />
+    </div>
   );
 };
 
