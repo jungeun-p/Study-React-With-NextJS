499e5887f3f4b45e273daa0e838f2fe4ec7a9cbe
jungeun-p <mrk7753@naver.com>
2023-03-18 15:37:19 +0900

    [ modify ] Add Create TodoItem function for creating todo
    - Create createTodo, createTodoDate, handleChange event function
    - return form jsx component

--- todo-mate/src/MainPage.js
+++ todo-mate/src/MainPage.js
@@ -22,7 +22,7 @@ const MainPage = () => {
       id: 3,
       date: "23-01-11",
       title: "Third",
-      contents: "ThirdContents",
+      contents: `There's nothing like threatening to go live with a bunch of Bill Murray pictures on a website to 'encourage' your clients to send you their content faster.`,
       completed: false,
     },
     {
@@ -48,9 +48,41 @@ const MainPage = () => {
     },
   ];
   const [todoData, setTodoData] = useState(initialize);
+  const [todoItem, setTodoItem] = useState({
+    id: "",
+    date: "",
+    title: "",
+    contents: "",
+    completed: false,
+  });
+
+  // Create TodoItem
+  const createTodo = (e) => {
+    e.preventDefault();
+    let newTodoItem = {
+      id: todoData.length + 1,
+      date: createTodayDate(),
+      title: todoItem.title,
+      contents: todoItem.contents,
+      completed: false,
+    };
+    setTodoData((prev) => [...prev, newTodoItem]);
+    setTodoItem("");
+  };
+
+  // Create TodoItem Date
+  const createTodayDate = () => {
+    const event = new Date();
+    return event.toDateString();
+  }
+
   return (
     <>
-      <TodoForm />
+      <TodoForm
+        todoItem={todoItem}
+        setTodoItem={setTodoItem}
+        createTodo={createTodo}
+      />
       <TodoList todoData={todoData} setTodoData={setTodoData} />
     </>
   );
--- todo-mate/src/components/Todo/TodoForm.js
+++ todo-mate/src/components/Todo/TodoForm.js
@@ -1,7 +1,31 @@
 import React from "react";
 
-const TodoForm = () => {
-  return <div>TodoForm</div>;
+const TodoForm = ({ todoItem, setTodoItem, createTodo }) => {  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTodoItem({ ...todoItem, [name]: value });
+  };
+  return (
+    <div className="todoFormContainer">
+      <form onSubmit={createTodo}>
+        <input
+          type="text"
+          name="title"
+          value={todoItem.title || ""}
+          placeholder="title"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="contents"
+          value={todoItem.contents || ""}
+          placeholder="contents"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Add✔️" />
+      </form>
+    </div>
+  );
 };
 
 export default TodoForm;
