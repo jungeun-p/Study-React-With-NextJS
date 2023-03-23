ed367505611a98f63a791ab07932d79f778a9f10
jungeun-p <mrk7753@naver.com>
2023-03-23 21:39:30 +0900

    [ modify ] Update TodoForm jsx component and Styling
    - Update TodoForm.css file for checking mood
    - Add input element for checking mood

--- todo-mate/src/components/Todo/TodoForm.js
+++ todo-mate/src/components/Todo/TodoForm.js
@@ -1,14 +1,53 @@
-import React from "react";
-import './TodoForm.css';
+import React, { useState } from "react";
+import "./TodoForm.css";
 
 const TodoForm = ({ todoItem, setTodoItem, createTodoItem }) => {
+  const [checkedMood, setCheckedMood] = useState(false);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setTodoItem({ ...todoItem, [name]: value });
   };
+
   return (
     <div className="todoFormContainer">
       <form className="todoForm" onSubmit={createTodoItem}>
+        <div className="formInput todoMoodWrapper">
+          <input
+            className={`todoMood ${checkedMood}`}
+            onClick={handleChange}
+            type="button"
+            name="mood"
+            value="🙂"
+          />
+          <input
+            className={`todoMood ${checkedMood}`}
+            onClick={handleChange}
+            type="button"
+            name="mood"
+            value="🥰"
+          />
+          <input
+            className="todoMood"
+            onClick={handleChange}
+            type="button"
+            name="mood"
+            value="☹️"
+          />
+          <input
+            className="todoMood"
+            onClick={handleChange}
+            type="button"
+            name="mood"
+            value="😵‍💫"
+          />
+          <input
+            className="todoMood"
+            onClick={handleChange}
+            type="button"
+            name="mood"
+            value="🥲"
+          />
+        </div>
         <input
           className="formInput"
           type="text"
