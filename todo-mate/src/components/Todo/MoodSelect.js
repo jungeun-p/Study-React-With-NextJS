b2a9852e6d4a9dbc100a16452ea6d637daf7e3ef
jungeun-p <mrk7753@naver.com>
2023-03-27 18:07:53 +0900

    [ modify ] Create MoodSelect and abstract from TodoForm Component

---
+++ todo-mate/src/components/Todo/MoodSelect.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import "./TodoForm.css";
+
+const MoodSelect = ({ checked, value, emoji, handleChange }) => {
+  return (
+      <button
+        className={`todoMood ${checked ? "checked" : ""}`}
+        onClick={handleChange}
+        type="button"
+        name="mood"
+        value={value}
+      >
+        {emoji}
+      </button>
+  );
+};
+
+export default MoodSelect;
