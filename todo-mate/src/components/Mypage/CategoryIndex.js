304d30c96e6ecb496f069fd9893adf9232b00735
jungeun-p <mrk7753@naver.com>
2023-03-27 18:07:23 +0900

    [ modify ] Change props name and value

--- todo-mate/src/components/Mypage/CategoryIndex.js
+++ todo-mate/src/components/Mypage/CategoryIndex.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-const CategoryIndex = ({ moodName, moodIcon, moodValue }) => {
+const CategoryIndex = ({ moodValue, moodEmoji, moodNum }) => {
   const navigate = useNavigate();
   return (
-    <div className="moodIndex" onClick={() => navigate(`/${moodName}`)}>
-      <div className="moodIcon">{moodIcon}</div>
-      <div className="moodTitle">{moodName}</div>
-      <div className="moodValue">({moodValue})</div>
+    <div className="moodIndex" onClick={() => navigate(`/${moodValue}`)}>
+      <div className="moodIcon">{moodEmoji}</div>
+      <div className="moodTitle">{moodValue}</div>
+      <div className="moodValue">({moodNum})</div>
     </div>
   );
 };
