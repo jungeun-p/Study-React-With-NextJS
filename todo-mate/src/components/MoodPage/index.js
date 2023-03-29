94bb2d762ed17dc5388f59732bb6cc3397c4a970
jungeun-p <mrk7753@naver.com>
2023-03-29 12:17:58 +0900

    [ modify ] Update moodPage Component styling
    - create moodPage.css file

--- todo-mate/src/components/MoodPage/index.js
+++ todo-mate/src/components/MoodPage/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import todoDataBase, { initialTodoData } from "../../assets/data";
+import { initialTodoData } from "../../assets/data";
 import MoodList from "./MoodList";
+import "./moodPage.css";
 
 const MoodPage = () => {
   const params = useParams();
   const moodData = initialTodoData.filter((todo) => todo.mood === params.moodName);
   return (
     <div className="moodPageContainer">
+      <div className="moodName">{params.moodName}</div>
       <MoodList moodData={moodData} />
     </div>
   );
