40fbf59300c83ba82034b0eff07ca89aa5d73b9f
jungeun-p <mrk7753@naver.com>
2023-04-26 21:45:44 +0900

    [ modify ] update axios instance config

--- todo-mate/src/api/axios.js
+++ todo-mate/src/api/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8090/api/collections/',
+  baseURL: "http://127.0.0.1:8090/api/collections/",
   params: {
     language: "ko-KR",
   },
+  responseType: "json",
+  headers: { "X-Requested-With": "XMLHttpRequest" },
 });
 
 export default instance;
