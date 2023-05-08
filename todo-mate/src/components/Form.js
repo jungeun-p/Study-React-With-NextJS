723df32797feb806bf5c95cae4b45c0fd75f8283
jungeun-p <mrk7753@naver.com>
2023-05-08 17:38:40 +0900

    [ modify ] create Form styled components

---
+++ todo-mate/src/components/Form.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const FormInput = styled.input`
+  border: none;
+  font-size: 15px;
+  padding: 10px;
+  margin: 5px 0 10px 0;
+  border-radius: 15px;
+  outline: none;
+  background-color: rgb(233, 233, 233);
+`;
+
+export const FormButton = styled.button`
+  background-color: black;
+  color: white;
+  outline: none;
+  border: none;
+  font-size: 15px;
+  font-weight: 600;
+  margin: 5px 0 10px 0;
+  padding: 12px 0;
+  border-radius: 10px;
+`;
