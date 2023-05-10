115eb5c400476ea7cbd0f646263eda439e681690
jungeun-p <mrk7753@naver.com>
2023-05-10 14:09:32 +0900

    [ modify ] create TDD 'push createMood button, open the createMoodForm'

--- todo-mate-tdd/src/App.test.js
+++ todo-mate-tdd/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 // moodlist에 7개의 todoitme이 존재한다.
@@ -8,9 +8,19 @@ test("moodData array lentgh is seven", () => {
   expect(length).toHaveTextContent("7");
 });
 
-// moodCreate button has "?" content
+// createMood button has "?" content
 test('moodCreate button has "?" content', () => {
   render(<App />);
   const question = screen.getByTestId("createMood");
   expect(question).toHaveTextContent("？");
 });
+
+// if push createMood button, open the createMoodForm
+
+test('if push createMood button, open the createMoodform', () => {
+  render(<App />);
+  const button = screen.getByTestId("createMood");
+  fireEvent.click(button);
+  const form = screen.getByTestId("createForm");
+  expect(form).toBeEnabled();
+})
