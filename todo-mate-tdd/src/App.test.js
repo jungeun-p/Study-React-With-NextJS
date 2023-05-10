4df61921f196be46511642cfebb1cc7891311b9a
jungeun-p <mrk7753@naver.com>
2023-05-10 13:35:25 +0900

    [ modify ] create TDD 'moodCreate button has "?" content'

--- todo-mate-tdd/src/App.test.js
+++ todo-mate-tdd/src/App.test.js
@@ -1,9 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-// todolist에 3개의 todoitme이 존재한다. 
-test('todoData array lentgh is three', () => {
+// moodlist에 7개의 todoitme이 존재한다.
+test("moodData array lentgh is seven", () => {
   render(<App />);
-  const length = screen.getByTestId("todoDataLength");
-  expect(length).toHaveTextContent("3");
+  const length = screen.getByTestId("moodDataLength");
+  expect(length).toHaveTextContent("7");
+});
+
+// moodCreate button has "?" content
+test('moodCreate button has "?" content', () => {
+  render(<App />);
+  const question = screen.getByTestId("createMood");
+  expect(question).toHaveTextContent("？");
 });
