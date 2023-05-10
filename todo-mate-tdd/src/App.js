47388dc0d8dd061f54f2e244357fe98bca862a26
jungeun-p <mrk7753@naver.com>
2023-05-10 13:36:03 +0900

    [ modify ] change state name 'todo' to 'mood'
    - Apply styled Components
    - create createMood Button
    - add moodData

--- todo-mate-tdd/src/App.js
+++ todo-mate-tdd/src/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import styled from "styled-components";
 import "./App.css";
 
 function App() {
-  const [todoData, setTodoData] = useState([
+  const [moodData, setMoodData] = useState([
     {
       id: 1,
       mood: "🥰",
@@ -18,12 +19,65 @@ function App() {
       mood: "😡",
       content: "mad",
     },
+    {
+      id: 4,
+      mood: "😞",
+      content: "gloomy",
+    },
+    {
+      id: 5,
+      mood: "😌",
+      content: "peaceful",
+    },
+    {
+      id: 6,
+      mood: "😵‍💫",
+      content: "confuse",
+    },
+    {
+      id: 7,
+      mood: "🤓",
+      content: "concentrate",
+    },
   ]);
+  
   return (
-    <div className="App">
-      <div data-testid="todoDataLength">{todoData.length}</div>
-    </div>
+    <MoodDiaryWrapper>
+      <TopRow>
+        <h3>😶</h3>
+        <h3>Mood Diary</h3>
+        <div data-testid="moodDataLength">({moodData.length})</div>
+      </TopRow>
+      <BottomRow>
+        {moodData.map((item) => (
+          <div key={item.id}>{item.mood}</div>
+        ))}
+        <div className="createMood" data-testid="createMood">？</div>
+      </BottomRow>
+    </MoodDiaryWrapper>
   );
 }
+
+const MoodDiaryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TopRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+`;
+
+const BottomRow = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  row-gap: 10px;
+  align-items: center;
+  justify-items: center;
+  padding: 10px 0;
+`;
 
 export default App;
